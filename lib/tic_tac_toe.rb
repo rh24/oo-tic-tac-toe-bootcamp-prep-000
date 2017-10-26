ed9f5cb93fe7d016bf5da8a10a2747c19f2c8741
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-26 20:54:42 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -25,12 +25,12 @@ class TicTacToe
   def input_to_index(input)
     index = input.to_i - 1
   end
-  
+
   def index
     @index = index
   end
 
-  def move( token)
+  def move(token)
     @board[@index] = token
   end
 
