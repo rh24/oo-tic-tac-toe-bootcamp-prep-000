38cf46f08b17b4b10a85d5e135874c1c3c2dc79e
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-26 19:12:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,4 +14,111 @@ class TicTacToe
   [6,4,2]
 ]
 
+  def display_board(board)
+    row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+    row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+    row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+    separator = "-----------"
+
+    puts row1
+    puts separator
+    puts row2
+    puts separator
+    puts row3
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+
+  def move(board, index, token)
+    board[index] = token
+  end
+
+  def position_taken?(board, index)
+    board[index] != " "
+  end
+
+  def valid_move?(board, index)
+    !position_taken?(board, index) && index.between?(0, 8)
+  end
+
+  def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+      if valid_move?(board, index)
+        move(board, index, current_player(board))
+        display_board(board)
+      else
+        turn(board)
+      end
+  end
+
+  def turn_count(board)
+      turns = 0
+      board.each do |turn|
+        if turn != " "
+          turns += 1
+        end
+      end
+      turns
+  end
+
+  def current_player(board)
+    turn_count(board) % 2 == 0 ? "X" : "O"
+  end
+
+  def won?(board)
+    WIN_COMBINATIONS.each do |win|
+      if win.all?{|i| board[i] == "X"}
+        return win
+      elsif win.all?{|i| board[i] == "O"}
+        return win
+      end
+    end
+    return false
+  end
+
+  def full?(board)
+    board.none? do |token|
+      if token == " "
+        true
+      else
+        false
+      end
+    end
+  end
+
+  def draw?(board)
+    if won?(board)
+      false
+    elsif full?(board)
+      true
+    end
+  end
+
+  def over?(board)
+    if draw?(board)
+      return true
+    elsif won?(board)
+      return true
+    else
+      return false
+    end
+  end
+
+  def winner(board)
+    if won?(board)
+      WIN_COMBINATIONS.each do |win|
+        if win.all?{|i| board[i] == "X"}
+          return "X"
+        elsif win.all?{|i| board[i] == "O"}
+          return "O"
+        end
+      end
+    end
+    return nil
+  end
+
 end
