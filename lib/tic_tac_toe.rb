60cd3695b76923548592bc9a5477a98fbe4a129e
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-26 21:38:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ class TicTacToe
   end
 
   def move(index, token)
-    @board[index] = token
+    @board[index] = @token
   end
 
   def position_taken?(index)
@@ -43,7 +43,7 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
       if valid_move?(index)
-        move(index, token)
+        move(index, @token)
         display_board
       else
         turn
