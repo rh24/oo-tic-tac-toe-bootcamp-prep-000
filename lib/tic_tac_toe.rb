d7a30ef3117e4127922ffe89dfb990330480a5e4
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-26 21:40:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input)
       if valid_move?(index)
-        move(index, @token)
+        move(index, current_player)
         display_board
       else
         turn
