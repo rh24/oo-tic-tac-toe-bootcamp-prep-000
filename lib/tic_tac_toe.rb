0f2b4ec36832dd6d90b0cd9753a504059ab574cb
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-26 19:07:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,17 +14,17 @@ class TicTacToe
   [6,4,2
 ]
 
-def display_board(board)
-row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
-row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
-row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
-separator = "-----------"
+  def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  separator = "-----------"
 
-puts row1
-puts separator
-puts row2
-puts separator
-puts row3
-end
+  puts row1
+  puts separator
+  puts row2
+  puts separator
+  puts row3
+  end
 
 end
