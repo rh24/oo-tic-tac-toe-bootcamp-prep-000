f7c2d88f62e67393865683e9e92e1f03100651d6
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-26 19:22:03 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,16 +15,11 @@ class TicTacToe
 ]
 
   def display_board
-    row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
-    row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
-    row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
-    separator = "-----------"
-
-    puts row1
-    puts separator
-    puts row2
-    puts separator
-    puts row3
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
 
   def input_to_index(input)
