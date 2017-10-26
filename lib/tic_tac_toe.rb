cd5b2efc33ca7747f3b59ffbc0f399b0cb4e58f5
Rebecca Hong <ms.rebeccahong@gmail.com>
2017-10-26 21:43:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -121,7 +121,7 @@ class TicTacToe
         turn
     end
     if won?
-      puts "Congratulations #{winner(board)}!"
+      puts "Congratulations #{winner(@board)}!"
     elsif draw?
       puts "Cat\'s Game!"
     end
